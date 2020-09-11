9d9a241daf5782bbd409aa5c92e85e21c6b52769
Promise Fru <promisefru1@gmail.com>
2020-09-11 15:09:34 +0100

    modifications made

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+var dotenv = require('dotenv');
 
+dotenv.config();
 const cors = require('cors')
 
 const mongoose = require('mongoose')
